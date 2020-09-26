e3b9cfc74397582389bce56e32ac0a8a1038f3f4
vagrant <vagrant@localhost.localdomain>
2020-09-26 10:53:40 +0000

    Add question edit_destroy function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -23,18 +23,29 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to question_path(@question), notice: "問題を編集しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to mypage_questions_path
   end
 
   private
 
     def question_params
       params.require(:question).permit(:id, :user_id, :sentence, :image, :hint_1, :hint_2, :hint_3,
-                                        :answer, :commentary, :answer_image, :average_rating)
+                                        :answer, :commentary, :answer_image, :average_rating,
+                                        :remove_image, :remove_answer_image)
     end
 end
