65234d150f500d052e3c892513146222b03e7eb7
vagrant <vagrant@localhost.localdomain>
2020-09-28 11:09:51 +0000

    Fix migration

--- db/migrate/20200925043409_create_questions.rb
+++ db/migrate/20200925043409_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
-      t.references :users, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :sentence
       t.string :image_id
       t.string :hint_1
--- db/migrate/20200925043459_create_ratings.rb
+++ db/migrate/20200925043459_create_ratings.rb
@@ -1,8 +1,8 @@
 class CreateRatings < ActiveRecord::Migration[5.0]
   def change
     create_table :ratings do |t|
-      t.references :users, foreign_key: true
-      t.references :questions, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :question, foreign_key: true
       t.integer :value, null: false
       t.text :comment
 
