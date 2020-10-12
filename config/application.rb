ce6b6943ea59f6ef168499c3fdce27395ade91d5
vagrant <vagrant@localhost.localdomain>
2020-10-12 06:58:05 +0000

    Add error pages

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module NazoTalk
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     Refile.secret_key = 'a123b7e1b05e7f7084a34543e0e5ee12dd56a889955300d94ac6e9e7470ea1c673316a850ebc63e0a5a11a461525bc4d5f7cb99125fb3507bda544156fde12a9'
+    config.exceptions_app = ->(env) { ErrorsController.action(:show).call(env) }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
